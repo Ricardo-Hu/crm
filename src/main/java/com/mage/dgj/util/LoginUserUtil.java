69ce1f61f95a4739e546ac52e095dd03c0f76a43
HHX <948019472@qq.com>
2020-03-08 16:02:36 +0800

    DGJ项目 用户登录&退出&非法请求拦截$全局异常

---
+++ src/main/java/com/mage/dgj/util/LoginUserUtil.java
@@ -0,0 +1,23 @@
+package com.mage.dgj.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+public class LoginUserUtil {
+
+    /**
+     * 从cookie中获取userId
+     * @param request
+     * @return
+     */
+    public static int releaseUserIdFromCookie(HttpServletRequest request) {
+        String userIdString = CookieUtil.getCookieValue(request, "userIdStr");
+        if (StringUtils.isBlank(userIdString)) {
+            return 0;
+        }
+        Integer userId = UserIDBase64.decoderUserID(userIdString);
+        return userId;
+    }
+}
